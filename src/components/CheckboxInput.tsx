30c07ecc1c91a5966e531e15a556649dbebafa27
juanmderosa <juanmderosa@gmail.com>
2025-07-08 09:56:24 -0300

    Ajustes estilos

--- src/components/CheckboxInput.tsx
+++ src/components/CheckboxInput.tsx
@@ -27,7 +27,7 @@ export const CheckboxInput = ({ name, label }: Props) => {
           <input
             id={name}
             type="checkbox"
-            className="bg-white placeholder:text-gray-400 text-black px-2 py-2 rounded focus:outline-2 focus:outline-gray-100"
+            className="bg-white px-2 py-2 rounded-xs appearance-none h-3 w-3 border border-gray-400  checked:bg-amber-500 checked:border-white checked:border-2 checked:text-white checked:rounded-xs checked:h-1 checked:w-1 cursor-pointer"
             checked={field.value}
             onChange={(e) => field.onChange(e.target.checked)}
           />
